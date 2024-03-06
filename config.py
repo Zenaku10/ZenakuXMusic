24f73c36b67f7694b05782fbad5308fb297e6b3d
Zenaku10 <162084403+Zenaku10@users.noreply.github.com>
2024-03-06 11:26:03 +0630

    Update config.py

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "1200"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b3d0e737737d67a5bf5a5.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/5be4505a80556f0652b13.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/203f9e617d4bbe0627b97.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sastatony")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/team_ligit")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ZeNaKuCHaT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ZeNaKuTeCH")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://te.legra.ph/file/398dceb7be661343981dc.jpg"
