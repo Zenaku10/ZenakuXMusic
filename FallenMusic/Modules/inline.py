fc557d3062c17f538a7f66e44790dafeedd4d24b
Zenaku10 <162084403+Zenaku10@users.noreply.github.com>
2024-03-06 20:47:02 +0630

    Update inline.py

--- FallenMusic/Modules/inline.py
+++ FallenMusic/Modules/inline.py
@@ -1,24 +1,3 @@
-# MIT License
-#
-# Copyright (c) 2023 AnonymousX1025
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 from pyrogram.types import (
     InlineKeyboardButton,
@@ -45,7 +24,7 @@ async def inline_query_handler(_, query):
         except:
             return
     else:
-        a = VideosSearch(text, limit=20)
+        a = VideosSearch(text, limit=10)
         result = (await a.next()).get("result")
         for x in range(15):
             title = (result[x]["title"]).title()
